8f9933ed7d602e5da78b5b5b5209108dc66bc747
mohamedf1234 <139957142+mohamedf1234@users.noreply.github.com>
2025-12-15 00:04:49 +0200

    Update builtins.h

--- include/builtins.h
+++ include/builtins.h
@@ -5,6 +5,7 @@ int is_builtins(char** args, char* line, char* historyFilePath);
 void cd(char** args);
 void about();
 void help();
+void help_specific(char *cmd_name);
 void count(char** args);
 
 #endif
